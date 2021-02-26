cb0336794f3b8a07441de38e9a8df633a0ac6f09
rusad1team <79636026+rusad1team@users.noreply.github.com>
2021-02-26 20:07:55 +0300

    add preloader

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,34 @@
 "use strict";
 
+$(document).ready(preloderFunction());
+
+function preloderFunction() {
+  setTimeout(function () {
+    // Force Main page to show from the Start(Top) even if user scroll down on preloader - Primary (Before showing content)
+    // Model 1 - Fast            
+    document.getElementById("page-top").scrollIntoView(); // Model 2 - Smooth             
+    // document.getElementById("page-top").scrollIntoView({behavior: 'smooth'});
+    // Removing Preloader:
+
+    $('#ctn-preloader').addClass('loaded'); // Once the preloader has finished, the scroll appears 
+
+    $('body').removeClass('no-scroll-y');
+
+    if ($('#ctn-preloader').hasClass('loaded')) {
+      // It is so that once the preloader is gone, the entire preloader section will removed
+      $('#preloader').delay(1000).queue(function () {
+        $(this).remove(); // If you want to do something after removing preloader:
+
+        afterLoad();
+      });
+    }
+  }, 1500);
+}
+
+function afterLoad() {// After Load function body!
+} //Code by ARiyou2000
+
+
 $(document).ready(function () {
   document.addEventListener('mousemove', function (e) {
     return parallax(e);
@@ -65,6 +94,7 @@ $(document).ready(function () {
   $('.banner-slider').slick({
     arrows: false,
     fade: true,
+    lazyLoad: 'ondemand',
     dots: true,
     autoplay: false,
     adaptiveHeight: true
