9d8ac58a68ec7a7f8600e8310136a16a6208c383
rusad1team <79636026+rusad1team@users.noreply.github.com>
2021-05-13 12:20:35 +0300

    add label page

--- js/main.js
+++ js/main.js
@@ -43,7 +43,16 @@ $(document).ready(function () {
     });
   };
 
-  sanwichToggle(); //faq
+  sanwichToggle(); //scroll 
+
+  $('.scroll-btn').click(function () {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top - 60;
+    jQuery("html:not(:animated), body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  }); //faq
   // $('.faq-item__header').on('click', function() {
   //     $(this).next().slideToggle();
   // })
